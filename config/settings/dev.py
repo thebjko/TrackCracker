9441e0c9aedf5c5e10aca76129bc178b386f6f02
thebjko <thebjko@gmail.com>
2023-06-22 21:03:35 +0900

    [setup] project setup

---
+++ config/settings/dev.py
@@ -0,0 +1,9 @@
+from .base import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-59lmck$$l!9q974mx7*nc&m2y^_k#e3dz&7nub(cmwark#n(d5'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
