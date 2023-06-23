aa493f54b7405daac48c8b8dbfd934bc5f8a39e7
thebjko <thebjko@gmail.com>
2023-06-23 13:50:32 +0900

    [setup] model prototype

--- crackers/models.py
+++ crackers/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Objective(models.Model):
+    title = models.CharField('title', max_length=100)
+    description = models.TextField('description')
+
+    @property
+    def achievement(self):
+        pass
+
+
+class Subtask(models.Model):
+    objective = models.ForeignKey('crackers.Objective', verbose_name='objective', on_delete=models.CASCADE)
+    task = models.ForeignKey('self', verbose_name='subtask', on_delete=models.CASCADE, blank=True, null=True)
+
+    def save(self, **kwargs):
+        self.task
+        return super().save(**kwargs)
+    
+    @property
+    def achievement(self):
+        pass
