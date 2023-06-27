135a8f672966a8bbe063211fa1ebbc04d1adb68f
thebjko <thebjko@gmail.com>
2023-06-27 10:59:53 +0900

    [feat] link create from objective table

--- crackers/urls.py
+++ crackers/urls.py
@@ -8,11 +8,11 @@ urlpatterns = [
     path('create/', views.create, name='create'),   # create objective
 
     path('objective/<int:pk>/', views.tasks, name='tasks'),   # tasks under objective of pk
-    # path('objective/<int:pk>/create/', views.create_task, name='create_task'),    # create subtask under supertask
+    path('objective/<int:pk>/create/', views.create_task, name='create_task'),    # create subtask under supertask
+    path('objective/<int:pk>/redirect/', views.redirect_to_create_task, name='redirect_to_create_task'),
     # path('subtasks/<int:supertask_pk>/', views.subtasks, name='subtasks'),   # subtasks under supertask of pk
     # path('subtasks/<int:supertask_pk>/create/', views.create_task, name='create_task'),    # create subtask under supertask
     
-    path('<int:pk>/redirect/', views.redirect_to_create, name='redirect_to_create'),
     path('<int:pk>/detail/', views.detail, name='detail'),
     path('update/', views.update, name='update'),
     path('<int:pk>/update/', views.update, name='update'),
