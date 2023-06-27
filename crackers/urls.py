169d55d4c56c88d221df0fcc74476f7f94592ee6
thebjko <thebjko@gmail.com>
2023-06-27 15:01:35 +0900

    [feat] subtask list view

--- crackers/urls.py
+++ crackers/urls.py
@@ -6,14 +6,15 @@ app_name = 'tracks'
 urlpatterns = [
     path('', views.index, name='index'),   # all objectives
     path('create/', views.create, name='create'),   # create objective
+    path('objective/<int:objective_pk>/update/', views.update, name='update'),   # tasks under objective of pk
+    path('objective/<int:objective_pk>/delete/', views.delete, name='delete'),
 
     path('objective/<int:objective_pk>/', views.tasks, name='tasks'),   # tasks under objective of pk
     path('objective/<int:objective_pk>/detail/', views.detail, name='detail'),
     path('objective/<int:objective_pk>/create/', views.create_task, name='create_task'),    # create subtask under supertask
     path('objective/<int:objective_pk>/redirect/', views.redirect_to_create_task, name='redirect_to_create_task'),
-    # path('subtasks/<int:supertask_pk>/', views.subtasks, name='subtasks'),   # subtasks under supertask of pk
+    
+    path('subtasks/<int:supertask_pk>/', views.subtasks, name='subtasks'),   # subtasks under supertask of pk
     # path('subtasks/<int:supertask_pk>/create/', views.create_task, name='create_task'),    # create subtask under supertask
     
-    path('objective/<int:objective_pk>/update/', views.update, name='update'),   # tasks under objective of pk
-    path('objective/<int:objective_pk>/delete/', views.delete, name='delete'),
 ]
