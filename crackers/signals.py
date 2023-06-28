8fd84c7fce0849cce05df871591530465bca9392
thebjko <thebjko@gmail.com>
2023-06-28 14:47:40 +0900

    [doc] edit unnecessary comment

--- crackers/signals.py
+++ crackers/signals.py
@@ -7,8 +7,6 @@ from .models import Task
 
 @receiver([post_save, post_delete], sender=Task)
 def reassess_achievement(sender, instance, **kwargs):
-    # 어떻게 테스트 할 것인가?
-    # print('post save handler on', instance.pk)
     if instance.supertask is not None:
         weighted_achievement_total = instance.supertask.subtasks.annotate(
             weighted_achievement=F('achievement')*F('proportion')
@@ -17,4 +15,4 @@ def reassess_achievement(sender, instance, **kwargs):
         ).get('weighted_achievement_total', 0)
         total = instance.supertask.subtasks.aggregate(total=Sum('proportion', output_field=FloatField())).get('total')
         instance.supertask.achievement = weighted_achievement_total / total
-        instance.supertask.save()   # 여기서 호출된 save 메서드는 post_save 신호를 발생시키는가?
+        instance.supertask.save()   # 여기서 호출된 save 메서드 또한 post_save 신호를 발생시킨다.
