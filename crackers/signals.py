2e762d11ab7261eb36715be259ec6a9d3282cbad
thebjko <thebjko@gmail.com>
2023-06-28 14:36:13 +0900

    [feat] complete reassessment of achievement

--- crackers/signals.py
+++ crackers/signals.py
@@ -6,15 +6,15 @@ from .models import Task
 
 
 @receiver(post_save, sender=Task)
-def reassess_achievement(sender, task, **kwargs):
+def reassess_achievement(sender, instance, **kwargs):
     # 어떻게 테스트 할 것인가?
-    print('post save handler on', task.pk)
-    if task.supertask is not None:
-        weighted_achievement_total = task.supertask.subtasks.annotate(
+    # print('post save handler on', instance.pk)
+    if instance.supertask is not None:
+        weighted_achievement_total = instance.supertask.subtasks.annotate(
             weighted_achievement=F('achievement')*F('proportion')
         ).aggregate(
             weighted_achievement_total=Sum('weighted_achievement', output_field=FloatField())
         ).get('weighted_achievement_total', 0)
-        total = task.supertask.subtasks.aggregate(total=Sum('proportion', output_field=FloatField())).get('total')
-        task.supertask.acheivement = weighted_achievement_total / total
-        task.supertask.save()   # 여기서 호출된 save 메서드는 post_save 신호를 발생시키는가?
+        total = instance.supertask.subtasks.aggregate(total=Sum('proportion', output_field=FloatField())).get('total')
+        instance.supertask.acheivement = weighted_achievement_total / total
+        instance.supertask.save()   # 여기서 호출된 save 메서드는 post_save 신호를 발생시키는가?
