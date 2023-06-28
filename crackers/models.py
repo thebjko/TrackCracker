007f4e128e6163bbb4c3e989904c2cfb14b5cbea
thebjko <thebjko@gmail.com>
2023-06-29 08:38:23 +0900

    [feat] comment type field

--- crackers/models.py
+++ crackers/models.py
@@ -6,7 +6,6 @@ class Task(models.Model):
     title = models.CharField('title', max_length=100)
     description = models.TextField('description', null=True, blank=True)
     supertask = models.ForeignKey('self', verbose_name='supertask', on_delete=models.CASCADE, blank=True, null=True, related_name='subtasks')
-    type = models.CharField('type', max_length=50, null=True, blank=True, default=None)
     completed = models.BooleanField('completed', default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -14,6 +13,7 @@ class Task(models.Model):
     proportion = models.IntegerField('proportion', validators=[MinValueValidator(0)])   # 이 Task가 Supertask 또는 Objective에서 차지하는 비중
     achievement = models.FloatField('achievement', default=0, validators=[MaxValueValidator(1.0), MinValueValidator(0.0)])
     
+    # type = models.CharField('type', max_length=50, null=True, blank=True, default=None)
     # duration = models.DurationField('duration', null=True, blank=True)
     # objective = models.ForeignKey('crackers.Objective', verbose_name='objective', on_delete=models.CASCADE, related_name='tasks')
     # total = models.IntegerField('total', default=10_000)
