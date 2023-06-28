66c031192cdb12ee8618611f2ee2fc10958cb037
thebjko <thebjko@gmail.com>
2023-06-28 14:04:03 +0900

    [feat: not examined] add signal handler that reassesses achievement

--- crackers/handlers.py
+++ crackers/handlers.py
@@ -1,3 +1,4 @@
+from django.db.models import FloatField, F, Sum
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
@@ -6,5 +7,14 @@ from .models import Task
 
 @receiver(post_save, sender=Task)
 def reassess_achievement(sender, task, **kwargs):
-    print('post save handler')
-    task.supertask.subtasks
+    # 어떻게 테스트 할 것인가?
+    print('post save handler on', task.pk)
+    if task.supertask is not None:
+        weighted_achievement_total = task.supertask.subtasks.annotate(
+            weighted_achievement=F('achievement')*F('proportion')
+        ).aggregate(
+            weighted_achievement_total=Sum('weighted_achievement', output_field=FloatField())
+        ).get('weighted_achievement_total')
+        total = task.supertask.subtasks.aggregate(total=Sum('proportion', output_field=FloatField()))
+        task.supertask.acheivement = weighted_achievement_total / total
+        task.supertask.save()   # 여기서 호출된 save 메서드는 post_save 신호를 발생시키는가?
