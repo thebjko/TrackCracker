37b56b9c0c15221a51e075e548cc3a1b27357c12
thebjko <thebjko@gmail.com>
2023-06-29 16:09:45 +0900

    [feat] 현재 자기 자신의 progress는 변경되고 있음

--- static/eventHandlers.js
+++ static/eventHandlers.js
@@ -2,4 +2,9 @@ document.addEventListener('DOMContentLoaded', (_) => {
   document.body.addEventListener('change-offcanvas-title', (e) => {
     document.querySelector('.offcanvas-title').textContent = e.detail.title
   })
+  document.body.addEventListener('change-achievement-width', (e) => {
+    const progress = document.getElementById(e.detail.identifier)
+    progress.setAttribute('style', `width: ${e.detail.width}%`)
+    progress.textContent = e.detail.width + '% Achieved'
+  })
 })
