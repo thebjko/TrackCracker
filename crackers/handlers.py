108663297d68de9a772d40c2072116bd8f773a36
thebjko <thebjko@gmail.com>
2023-06-29 21:29:53 +0900

    [refactor] delete unnecessary comment

--- crackers/handlers.py
+++ crackers/handlers.py
@@ -10,21 +10,10 @@ def reassess_achievement(sender, **kwargs):
     supertask = kwargs.get('supertask')
     if supertask is not None and supertask.completed == False:
         supertask.achievement = supertask.assess_achievement()
-        # if supertask.subtasks.exists():
-        #     # 최상위 Task가 아닌 경우. 완료 표시가 된 경우 1.0으로 유지
-        #     weighted_achievement_total = supertask.subtasks.annotate(
-        #         weighted_achievement=F('pseudo_achievement')*F('proportion')
-        #     ).aggregate(
-        #         weighted_achievement_total=Sum('weighted_achievement', output_field=FloatField())
-        #     ).get('weighted_achievement_total', 0)
-        #     total = supertask.subtasks.aggregate(
-        #         total=Sum('proportion', output_field=FloatField())
-        #     ).get('total', 1)
-        #     supertask.achievement = weighted_achievement_total / total
-        # else:
-        #     supertask.achievement = 0.0
+        
         if supertask.achievement == 1.0:
             supertask.completed = True
         else:
             supertask.completed = False
+        
         supertask.save()   # 여기서 호출된 save 메서드 또한 post_save 신호를 발생시킨다.
