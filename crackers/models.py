f85000349eced09286bdf358362216dedade97d1
thebjko <thebjko@gmail.com>
2023-07-03 14:28:41 +0900

    [setup] add marked_complete field

--- crackers/models.py
+++ crackers/models.py
@@ -10,11 +10,13 @@ class Task(models.Model):
     description = models.TextField('description', null=True, blank=True)
     supertask = models.ForeignKey('self', verbose_name='supertask', on_delete=models.CASCADE, blank=True, null=True, related_name='subtasks')
     completed = models.BooleanField('completed', default=False)
+    marked_complete = models.BooleanField('marked complete', default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
     proportion = models.IntegerField('proportion', validators=[MinValueValidator(0)])   # 이 Task가 Supertask 또는 Objective에서 차지하는 비중
     achievement = models.FloatField('achievement', default=0, validators=[MaxValueValidator(1.0), MinValueValidator(0.0)])
+    # accumulative = models.BooleanField('accumulative', default=False)   # 서브태스크 생성시 proportion을 누적합으로 입력할 수 있게
     
     # type = models.CharField('type', max_length=50, null=True, blank=True, default=None)
     # duration = models.DurationField('duration', null=True, blank=True)
