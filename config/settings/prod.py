d9b3c5c086f22f99c0e8cfc84d511f90e9a97e36
thebjko <thebjko@gmail.com>
2023-07-03 15:08:41 +0900

    [setup] for production

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -7,3 +7,15 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
+
+DATABASES['default'] = {
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': os.getenv('DB_NAME'),
+    'USER': os.getenv('USERNAME'),
+    'PASSWORD': os.getenv('PASSWORD'),
+    'HOST': os.getenv('HOSTNAME'),
+    'PORT': os.getenv('RDS_PORT'),
+    'OPTIONS': {
+        'sql_mode': 'STRICT_ALL_TABLES'
+    },
+}
