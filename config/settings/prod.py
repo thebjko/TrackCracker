e2b634532d4c0179e009311b0dfceb579da45cb7
thebjko <thebjko@gmail.com>
2023-07-03 16:51:38 +0900

    [setup] for s3

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,5 +1,8 @@
 from .base import *
 
+
+import boto3
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
@@ -19,3 +22,13 @@ DATABASES['default'] = {
         'sql_mode': 'STRICT_ALL_TABLES'
     },
 }
+
+AWS_STORAGE_BUCKET_NAME = "supertodo-bucket-2023"
+
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+
+STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+
+AWS_S3_CUSTOM_DOMAIN = AWS_STORAGE_BUCKET_NAME + ".s3.ap-northeast-2.amazonaws.com"
+AWS_S3_FILE_OVERWRITE = False
+AWS_QUERYSTRING_AUTH = False
