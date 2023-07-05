cbd311859896463869cc91cbd811b44283547c62
thebjko <thebjko@gmail.com>
2023-07-05 13:21:13 +0900

    [feat] 하위태스크 achievement에 따라 현재 태스크 completed 변경

--- crackers/handlers.py
+++ crackers/handlers.py
@@ -10,4 +10,8 @@ def reassess_achievement(sender, **kwargs):
     if supertask is not None:
         supertask.achievement = supertask.assess_achievement()
         # 여기서 호출된 save 메서드 또한 post_save 신호를 발생시킨다.
+        if supertask.achievement == 1.0:
+            supertask.completed = True
+        else:
+            supertask.completed = False
         supertask.save()
