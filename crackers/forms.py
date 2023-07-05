9075d4f4f656b941bdfe8cbf75fa1ffbff5a730e
thebjko <thebjko@gmail.com>
2023-07-05 13:19:31 +0900

    [feat] accumulative field on forms prototype

--- crackers/forms.py
+++ crackers/forms.py
@@ -5,23 +5,25 @@ from django.forms.utils import ErrorList
 from .models import Task
 
 class TaskForm(forms.ModelForm):
+    # accumulative = forms.CheckboxInput(widge)
     class Meta:
         model = Task
         fields = (
             'title',
             'description',
             'proportion',
+            # 'accumulative',
         )
     
-    def clean_proportion(self):
-        proportion = self.cleaned_data.get('proportion')
-        supertask = self.cleaned_data.get('supertask')
-        if supertask is None and proportion is None:
-            return 10_000
-        elif supertask is not None and proportion is None:
-            self._errors['proportion'] = ErrorList(["Proportion is required when supertask is not None."])
-            raise ValidationError("Proportion is required when supertask is not None.")
-        return proportion
+    # def clean_proportion(self):
+    #     proportion = self.cleaned_data.get('proportion')
+    #     supertask = self.cleaned_data.get('supertask')
+    #     if supertask is None and proportion is None:
+    #         return 10_000
+    #     elif supertask is not None and proportion is None:
+    #         self._errors['proportion'] = ErrorList(["Proportion is required when supertask is not None."])
+    #         raise ValidationError("Proportion is required when supertask is not None.")
+    #     return proportion
 
     
 class TaskFormHelper(forms.Form):
