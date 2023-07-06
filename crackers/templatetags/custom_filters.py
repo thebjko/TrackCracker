f6d7072f7e3ab9885c549b0db3e0abea3ad096d2
thebjko <thebjko@gmail.com>
2023-07-06 10:29:43 +0900

    [feat] add markdown custom filter

--- crackers/templatetags/custom_filters.py
+++ crackers/templatetags/custom_filters.py
@@ -1,7 +1,15 @@
+import markdown as md
+
 from django import template
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
 @register.filter
 def all_completed(obj):
     return obj.subtasks.exists() and not obj.subtasks.filter(completed=False).exists()
+
+@register.filter
+def markdown(value):
+    extensions = ["nl2br", "fenced_code"]
+    return mark_safe(md.markdown(value, extensions=extensions))
