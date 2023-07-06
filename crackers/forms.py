98e435a60c8b1bc6df6883b89aa1a1f61eac425a
thebjko <thebjko@gmail.com>
2023-07-06 12:06:35 +0900

    [feat] accumulative not required

--- crackers/forms.py
+++ crackers/forms.py
@@ -6,7 +6,7 @@ class TaskForm(forms.ModelForm):
     proportion = forms.IntegerField(required=True, label='비중')
     description = forms.CharField(required=False, label='설명')
     title = forms.CharField(required=True, label='태스크 제목')
-    accumulative = forms.BooleanField(required=True, label=' 👈 서브태스크의 비중을 누적으로 입력하기 위해 체크하세요', widget=forms.CheckboxInput(attrs={'class': 'checkbox'}))
+    accumulative = forms.BooleanField(required=False, label=' 👈 서브태스크의 비중을 누적으로 입력하기 위해 체크하세요', widget=forms.CheckboxInput(attrs={'class': 'checkbox'}))
 
     class Meta:
         model = Task
