e0776d96564b822bafaa89498a595c1825d4944e
thebjko <thebjko@gmail.com>
2023-07-10 21:44:11 +0900

    [fix] modify utils

--- crackers/static/crackers/utils.js
+++ crackers/static/crackers/utils.js
@@ -1,5 +1,5 @@
 const toggleBtn = (targetId, btnId) => {
   document.getElementById(targetId).classList.toggle('d-none')
   const btn = document.getElementById(btnId)
-  btn.textContent == 'Close' ? btn.textContent = 'Detail' : btn.textContent = 'Close'
+  btn.textContent === 'Close' ? btn.textContent = 'Detail' : btn.textContent = 'Close'
 }
