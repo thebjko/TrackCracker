374b3a632399c783c126c6c3f3780f7c415140aa
thebjko <thebjko@gmail.com>
2023-07-10 21:38:23 +0900

    [feat] btn change

--- crackers/static/crackers/utils.js
+++ crackers/static/crackers/utils.js
@@ -1,5 +1,5 @@
 const toggleBtn = (targetId, btnId) => {
   document.getElementById(targetId).classList.toggle('d-none')
   const btn = document.getElementById(btnId)
-  btn.textContent == '닫기' ? btn.textContent = '자세히 보기' : btn.textContent = '닫기'
+  btn.textContent == 'Close' ? btn.textContent = 'Detail' : btn.textContent = 'Close'
 }
