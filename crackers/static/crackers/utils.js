95183ca7c7d9a1d23906c7c872bc649aaa58a43c
thebjko <thebjko@gmail.com>
2023-08-01 12:37:12 +0900

    [feat] add select-all functionality

--- crackers/static/crackers/utils.js
+++ crackers/static/crackers/utils.js
@@ -3,3 +3,25 @@ const toggleBtn = (targetId, btnId) => {
   const btn = document.getElementById(btnId)
   btn.textContent === 'Close' ? btn.textContent = 'Detail' : btn.textContent = 'Close'
 }
+
+const selectAll = () => {
+  const all = document.getElementById('select-all')
+  const boxes = document.querySelectorAll('[id^=delete-checkbox-for]')
+  boxes.forEach((box) => {
+    box.checked = all.checked
+  })
+}
+
+const allChecked = () => {
+  const boxes = document.querySelectorAll('[id^=delete-checkbox-for]')
+  let allSelected = true
+  boxes.forEach((box) => {
+    allSelected &= box.checked
+  })
+  return allSelected
+}
+
+const updateSelectAll = () => {
+  const all = document.getElementById('select-all')
+  all.checked = allChecked()
+}
